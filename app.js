e0271a703bee3b6da8c1f040f73955bd05846acb
MaxLyFe <maximilianolyfbuzios@gmail.com>
2022-09-22 21:27:32 -0300

    app.js and hex.html

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const color = document.querySelector('.color');
 btn.addEventListener('click', () =>{
     const randomNumber = getRandomNumbet();
     console.log(randomNumber);
+
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
 
 const getRandomNumbet = () => {
-    return Math.random();
+    return Math.floor(Math.random() * colors.length);
 };
